0edca1f6a1f3725460f3cb3261ec329b760eebae
Edgar Pino <me@edgarpino.com>
2017-05-03 16:39:51 -0500

    Initial commit

---
+++ webpack.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+    devtool: "source-map",
+    entry: '',
+    module: {
+        rules: [{
+            test: /\.scss$/,
+            use: [{
+                loader: "style-loader" // creates style nodes from JS strings
+            }, {
+                loader: "css-loader" // translates CSS into CommonJS
+            }, {
+                loader: "sass-loader" // compiles Sass to CSS
+            }]
+        }]
+    }
+};
